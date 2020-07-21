174b20efcfd3786739cd94cb8380b8320fcc43ce
liukun <liukun@mobimedical.com>
2020-07-21 10:27:45 +0800

    添加一些实用函数的实现方式

---
+++ js/dateFormat.js
@@ -0,0 +1,59 @@
+
+class dateFormat{
+    /**
+     * 获取开始时间到结束时间之间的所有日期
+     * @param {number} start 毫秒时间
+     * @param {number} end  毫秒时间
+     * @return {Array} dateArr
+     */
+    async getAllDate(start,end){
+        let startTime=new Date(start),endTime=new Date(end);
+        let dateArr=[];
+        while( (end - start) >0){
+            let  year = startTime.getFullYear();
+            let month= startTime.getMonth().toString().length===1?"0" + (parseInt(startTime.getMonth().toString(),10) + 1) : (startTime.getMonth() + 1);
+            let day = startTime.getDate().toString().length === 1 ? "0" + startTime.getDate() : startTime.getDate();
+            dateArr.push(year + "-" + month + "-" + day);
+            startTime.setDate(startTime.getDate()+1);
+            start = startTime.getTime();
+        }
+        return dateArr;
+    }
+
+     /**
+     * 根据对应的选项，计算相应的开始毫秒时和结束毫秒时
+     * @param {string} dateType
+     * @return {Array} dateArr 
+     */
+    async getDuration(dateType)
+    {
+        let startTime=0,endTime=Date.now();
+        let date = new Date();
+        let dateArr = [];
+        switch(dateType){
+            case 'today':
+                date.setHours(0,0,0,0);
+                startTime=date.getTime();
+                break;
+            case 'yesterday':
+                date.setHours(0,0,0,0);
+                endTime=date.getTime()-1;
+                startTime=endTime+1-24*60*60*1000;
+                break;
+            case 'week':
+                let day = date.getDay()-1;
+                date.setHours(0,0,0,0);
+                startTime=date.getTime()-day*24*60*60*1000;
+                break;
+            case 'month':
+                date.setDate(1);
+                date.setHours(0,0,0,0);
+                startTime=date.getTime();
+                break;
+        }
+        dateArr.push(startTime);
+        dateArr.push(endTime);
+        return dateArr;
+    }
+}
+module.exports = dateFormat;
---
+++ js/utils.js
@@ -0,0 +1,57 @@
+class utils{
+    /**
+     * 生成指定长度的随机字符串
+     * @param {array} optArray [n,l,m]
+     * @param {number} len 8
+     */
+    async RandomStr(optArray,len){
+        const numbers='0123456789';
+        const lowerLetters='abcdefgijklmnopqrstuvwxy';
+        const upperLetters=lowerLetters.toUpperCase();
+        let data='';
+        optArray.forEach(element=>{
+            if(element==='n'){
+                data += numbers;
+            }
+            if (element==='l'){
+                data += lowerLetters;
+            }
+            if (element==='u')
+            {
+                data += upperLetters;
+            }
+        });
+        let  res='';
+        const datalen=data.length;
+        for (let i = 0; i < len; i++) {
+            let random=Math.floor(Math.random()*datalen);
+            res += data[random];
+        }
+        return res;
+    }
+    /**
+     * sleep函数的实现方式
+     * @param {number} ms 毫秒
+     */
+    async sleep(ms){
+        return new Promise(resolve => setTimeout(resolve, ms))
+    }
+    /**
+     * 计算两个经纬度相差的距离,只精确到米
+     * @param {Number} lng1 第一个经度
+     * @param {Number} lat1 第一个维度
+     * @param {Number} lng2 第二个经度
+     * @param {Number} lat2 第二个维度
+     */
+    async distanceByLnglat(lng1, lat1, lng2, lat2){
+        let radLat1 = lat1 * Math.PI / 180.0;
+	    let radLat2 = lat2 * Math.PI / 180.0;
+        let a = radLat1 - radLat2;
+        let b = lng1 * Math.PI / 180.0 - lng2 * Math.PI / 180.0;
+        let s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+		s = s * 6378137.0; // 取WGS84标准参考椭球中的地球长半径(单位:m)
+		s = Math.round(s);
+		return s;
+    }
+}
+module.exports = utils;
