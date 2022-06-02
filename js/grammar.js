7e2672b0ccf04b263d0b095b1ca2b49e2993bf9e
oneWalker <lewkun@foxmail.com>
2022-06-03 00:39:14 +0800

    format

--- js/grammar.js
+++ js/grammar.js
@@ -42,7 +42,9 @@ function sleepGenerator(time) {
 function sleep(time) {
 	return new Promise(resolve =>
  	 setTimeout(resolve,time)
- ) } async function output() {
+ ) } 
+ 
+ async function output() {
 	let out = await sleep(1000); 
 	console.log(1); 
 	return out;
