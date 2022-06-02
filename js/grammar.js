6e8f187b33c983d5204df88d7992021f5755ae84
oneWalker <lewkun@foxmail.com>
2022-06-03 00:05:44 +0800

    add：different grammar version compare

---
+++ js/grammar.js
@@ -0,0 +1,50 @@
+//通过sleep函数进对各个版本的语法进行比较
+
+//1.ES5下通过时间（禁止，会导致CPU阻塞）
+function sleep(delay) {
+    var start = (new Date()).getTime();
+    while ((new Date()).getTime() - start < delay) {
+      continue;
+    }
+}
+function test() {
+    console.log('111');
+    sleep(2000);
+    console.log('222');
+}
+test()
+
+//2.ES5下通过setTimeout，callback
+function sleep1(ms, callback) {
+    setTimeout(callback, ms)
+}
+//sleep 1s
+sleep1(1000, () => {
+    console.log(1000)
+})
+
+
+//3.ES6下通过Promise
+const sleep = time => {
+    return new Promise(resolve => setTimeout(resolve,time));
+} 
+sleep(1000).then(()=>{ console.log(1) })
+
+//4.ES6:yield后面是一个生成器 generator
+function sleepGenerator(time) {
+	yield new Promise(function(resolve,reject){
+		setTimeout(resolve,time);
+	}) 
+} 
+
+
+//5.ES7下通过async/await
+function sleep(time) {
+	return new Promise(resolve =>
+ 	 setTimeout(resolve,time)
+ ) } async function output() {
+	let out = await sleep(1000); 
+	console.log(1); 
+	return out;
+} 
+output();
